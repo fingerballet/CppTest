a22a49426fe6cfea79609161ab2473d217ea3ddd
Administrator <administrator@ubuntu.ubuntu-domain>
2013-05-02 22:35:11 +0800

    change the folder relation

--- project/test.rb
+++ project/test.rb
@@ -5,10 +5,12 @@ Dir.chdir 'build'
 system 'cmake -G"Eclipse CDT4 - Unix Makefiles" ..'
 system 'cmake --build .'
 
-if File.exist? './CppTest' then
+if File.exist? 'CppTest' then
   File.chmod( 777, 'CppTest')
   system './CppTest'
+  puts "Exec OK!"
 else
+  puts "Exec Fail!"
   -1
 end
 
