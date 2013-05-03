865df9fe5e7bc0d260a77a0e68512e061ba7e9a5
Administrator <administrator@ubuntu.ubuntu-domain>
2013-05-03 22:58:41 +0800

    modify the ruby script

--- project/test.rb
+++ project/test.rb
@@ -1,4 +1,7 @@
-system 'rm -rf build' if File.directory? 'build'
+require "fileutils"
+
+#system 'rm -rf build' if File.directory? 'build'
+FileUtils.rm_rf 'build'
 Dir.mkdir 'build'
 Dir.chdir 'build'
 
@@ -6,11 +9,10 @@ system 'cmake -G"Eclipse CDT4 - Unix Makefiles" ..'
 system 'cmake --build .'
 
 if File.exist? 'CppTest' then
-  File.chmod( 777, 'CppTest')
+  system 'chmod 777, CppTest'
   system './CppTest'
-  puts "Exec OK!"
 else
-  puts "Exec Fail!"
+  puts "Failure!"
   -1
 end
 
